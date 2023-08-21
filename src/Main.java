901253812ce256b4081869b1e40febc2e984ffa9
Cl1yMba <petrmanastryly199@gmail.com>
2023-08-21 20:56:37 +0300

    Task 8

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String[] args) {
        //Task2
-        var firstBoxerWeight = 78.2;
-        var secondBoxerWeight = 82.7;
-        var subtractedWeights = secondBoxerWeight - firstBoxerWeight;
-        System.out.println(subtractedWeights);
-        var differencedWeights = secondBoxerWeight % firstBoxerWeight;
-        System.out.println(differencedWeights);
+        var workHours = 640;
+        var employeeWorkingHours = 8;
+        var employeesInCompany = workHours / employeeWorkingHours;
+        System.out.println("Всего работников в компании - " + employeesInCompany + " человек");
+        employeesInCompany += 94;
+        employeeWorkingHours = workHours/employeesInCompany;
+        System.out.println("Если в компании работает " + employeesInCompany + " человек, то всего " + employeeWorkingHours + " часов работы может быть поделено" +
+                " между сотрудниками");
 
 
 
